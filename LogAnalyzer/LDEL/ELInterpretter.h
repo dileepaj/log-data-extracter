0384a2bd42aca95877a1b9ac51c2fa0bb86968f8
Michelle <msumalini@gmail.com>
2020-03-15 16:04:40 +0530

    Removed .idea, .vs .vscode build and the cmake build file and added the new changes

--- LogAnalyzer/LDEL/ELInterpretter.h
+++ LogAnalyzer/LDEL/ELInterpretter.h
@@ -38,14 +38,16 @@ class ELInterpretter {
 public:
     ELInterpretterResult* EvaluateCase(MSTRING sDefFile);
     VEC_ELLINEANNOTATION* AnnotateAgainstLineTemplate(MSTRING sDefFile, MSTRING sLineTemplateName);
-    
+
 private:
     WIDESTRING ProcessLinesInFile(MSTRING sLogFile, VEC_ELLINETEMPLATE& vecLineTemplates, ELInterpretterResult *res);
     WIDESTRING ProcessBlocks(WIDESTRING str, VEC_ELBLOCKTEMPLATE& blockTemplates, VEC_ELLINETEMPLATE& lineTemplates, ELInterpretterResult *res);
     void UnifyBlock(WIDESTRING& str, ELBlockTemplate *block, ELInterpretterResult *res);
+    void UnifyBlockNonDefinite(WIDESTRING &str, ELBlockTemplate *block, ELInterpretterResult *res);
     void PrintInterpretterResult(ELInterpretterResult *ir);
     VEC_ELBLOCKTEMPLATE GetBlockTemplatesPreparedToHandleRecursiveDefs(VEC_ELBLOCKTEMPLATE& blockTemplates);
     void FillAnnotationElements(PNODE node, ELLineAnnotation *an, MSTRING::size_type& startPos);
+    bool findMultilineJSON(MSTRING sLine);
 };
 
 #endif /* defined(__LogAnalyzer__ELInterpretter__) */
